8adf88eebcdcb85cb037d039beaffc9bb61ad2b4
DogukanTekin <95180909+DogukanTekin@users.noreply.github.com>
2022-04-06 02:15:14 +0300

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,494 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.zorunlucheckbox = New System.Windows.Forms.CheckedListBox()
+        Me.secmelicheckbox = New System.Windows.Forms.CheckedListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.zorunlulabel = New System.Windows.Forms.Label()
+        Me.secmelilabel = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.zorunlu_ekle_buton = New System.Windows.Forms.Button()
+        Me.zorunlu_sil_buton = New System.Windows.Forms.Button()
+        Me.secmeli_sil_buton = New System.Windows.Forms.Button()
+        Me.secmeli_ekle_buton = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox6 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'zorunlucheckbox
+        '
+        Me.zorunlucheckbox.FormattingEnabled = True
+        Me.zorunlucheckbox.Location = New System.Drawing.Point(249, 28)
+        Me.zorunlucheckbox.Name = "zorunlucheckbox"
+        Me.zorunlucheckbox.Size = New System.Drawing.Size(386, 256)
+        Me.zorunlucheckbox.TabIndex = 0
+        '
+        'secmelicheckbox
+        '
+        Me.secmelicheckbox.FormattingEnabled = True
+        Me.secmelicheckbox.Location = New System.Drawing.Point(672, 28)
+        Me.secmelicheckbox.Name = "secmelicheckbox"
+        Me.secmelicheckbox.Size = New System.Drawing.Size(386, 256)
+        Me.secmelicheckbox.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(249, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(88, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Zorunlu Dersler"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(672, 10)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(87, 15)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Seçmeli Dersler"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(505, 10)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(83, 15)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Seçilen Kredi : "
+        '
+        'zorunlulabel
+        '
+        Me.zorunlulabel.AutoSize = True
+        Me.zorunlulabel.Location = New System.Drawing.Point(594, 10)
+        Me.zorunlulabel.Name = "zorunlulabel"
+        Me.zorunlulabel.Size = New System.Drawing.Size(13, 15)
+        Me.zorunlulabel.TabIndex = 4
+        Me.zorunlulabel.Text = "0"
+        '
+        'secmelilabel
+        '
+        Me.secmelilabel.AutoSize = True
+        Me.secmelilabel.Location = New System.Drawing.Point(1017, 10)
+        Me.secmelilabel.Name = "secmelilabel"
+        Me.secmelilabel.Size = New System.Drawing.Size(13, 15)
+        Me.secmelilabel.TabIndex = 9
+        Me.secmelilabel.Text = "0"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(8, 73)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(20, 15)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "TC"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(8, 102)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(51, 15)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Okul No"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(8, 131)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(22, 15)
+        Me.Label8.TabIndex = 6
+        Me.Label8.Text = "Ad"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(8, 160)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(39, 15)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "Soyad"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(8, 187)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(30, 15)
+        Me.Label10.TabIndex = 16
+        Me.Label10.Text = "Sınıf"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(8, 216)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(33, 15)
+        Me.Label11.TabIndex = 15
+        Me.Label11.Text = "Şube"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(8, 245)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(46, 15)
+        Me.Label12.TabIndex = 14
+        Me.Label12.Text = "Dönem"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(588, 471)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(47, 15)
+        Me.Label13.TabIndex = 13
+        Me.Label13.Text = "Label13"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(588, 435)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(47, 15)
+        Me.Label14.TabIndex = 12
+        Me.Label14.Text = "Label14"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(588, 402)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(47, 15)
+        Me.Label15.TabIndex = 11
+        Me.Label15.Text = "Label15"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(7, 11)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(207, 45)
+        Me.Label16.TabIndex = 10
+        Me.Label16.Text = "Yeni öğrenci kaydı için dersleri seçiniz." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "   Öğrenci bilgilerini girdikten sonra" &
+    " " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "       ""KAYDET"" Butonuna tıklayınız."
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(928, 10)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(83, 15)
+        Me.Label17.TabIndex = 18
+        Me.Label17.Text = "Seçilen Kredi : "
+        '
+        'zorunlu_ekle_buton
+        '
+        Me.zorunlu_ekle_buton.Location = New System.Drawing.Point(249, 290)
+        Me.zorunlu_ekle_buton.Name = "zorunlu_ekle_buton"
+        Me.zorunlu_ekle_buton.Size = New System.Drawing.Size(75, 23)
+        Me.zorunlu_ekle_buton.TabIndex = 19
+        Me.zorunlu_ekle_buton.Text = "EKLE"
+        Me.zorunlu_ekle_buton.UseVisualStyleBackColor = True
+        '
+        'zorunlu_sil_buton
+        '
+        Me.zorunlu_sil_buton.Location = New System.Drawing.Point(330, 290)
+        Me.zorunlu_sil_buton.Name = "zorunlu_sil_buton"
+        Me.zorunlu_sil_buton.Size = New System.Drawing.Size(75, 23)
+        Me.zorunlu_sil_buton.TabIndex = 20
+        Me.zorunlu_sil_buton.Text = "SİL"
+        Me.zorunlu_sil_buton.UseVisualStyleBackColor = True
+        '
+        'secmeli_sil_buton
+        '
+        Me.secmeli_sil_buton.Location = New System.Drawing.Point(753, 290)
+        Me.secmeli_sil_buton.Name = "secmeli_sil_buton"
+        Me.secmeli_sil_buton.Size = New System.Drawing.Size(75, 23)
+        Me.secmeli_sil_buton.TabIndex = 22
+        Me.secmeli_sil_buton.Text = "SİL"
+        Me.secmeli_sil_buton.UseVisualStyleBackColor = True
+        '
+        'secmeli_ekle_buton
+        '
+        Me.secmeli_ekle_buton.Location = New System.Drawing.Point(672, 290)
+        Me.secmeli_ekle_buton.Name = "secmeli_ekle_buton"
+        Me.secmeli_ekle_buton.Size = New System.Drawing.Size(75, 23)
+        Me.secmeli_ekle_buton.TabIndex = 21
+        Me.secmeli_ekle_buton.Text = "EKLE"
+        Me.secmeli_ekle_buton.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(65, 70)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox1.TabIndex = 23
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(65, 99)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox2.TabIndex = 24
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(65, 128)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox3.TabIndex = 25
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(65, 157)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox4.TabIndex = 26
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(65, 186)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(32, 19)
+        Me.CheckBox1.TabIndex = 27
+        Me.CheckBox1.Text = "1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(103, 186)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(32, 19)
+        Me.CheckBox2.TabIndex = 28
+        Me.CheckBox2.Text = "2"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(65, 215)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(34, 19)
+        Me.CheckBox3.TabIndex = 29
+        Me.CheckBox3.Text = "A"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(103, 215)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(33, 19)
+        Me.CheckBox4.TabIndex = 30
+        Me.CheckBox4.Text = "B"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.Location = New System.Drawing.Point(65, 244)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(46, 19)
+        Me.CheckBox5.TabIndex = 31
+        Me.CheckBox5.Text = "Güz"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'CheckBox6
+        '
+        Me.CheckBox6.AutoSize = True
+        Me.CheckBox6.Location = New System.Drawing.Point(117, 244)
+        Me.CheckBox6.Name = "CheckBox6"
+        Me.CheckBox6.Size = New System.Drawing.Size(56, 19)
+        Me.CheckBox6.TabIndex = 32
+        Me.CheckBox6.Text = "Bahar"
+        Me.CheckBox6.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(65, 282)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 33
+        Me.Button1.Text = "KAYDET"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(202, 363)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 34
+        Me.Button2.Text = "GÖSTER"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(12, 392)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(445, 199)
+        Me.ListBox1.TabIndex = 35
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Vize2Sınavı.My.Resources.Resources.deu
+        Me.PictureBox1.Location = New System.Drawing.Point(762, 371)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(234, 220)
+        Me.PictureBox1.TabIndex = 36
+        Me.PictureBox1.TabStop = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(544, 291)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 37
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1087, 633)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox6)
+        Me.Controls.Add(Me.CheckBox5)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.secmeli_sil_buton)
+        Me.Controls.Add(Me.secmeli_ekle_buton)
+        Me.Controls.Add(Me.zorunlu_sil_buton)
+        Me.Controls.Add(Me.zorunlu_ekle_buton)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.secmelilabel)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.zorunlulabel)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.secmelicheckbox)
+        Me.Controls.Add(Me.zorunlucheckbox)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents zorunlucheckbox As CheckedListBox
+    Friend WithEvents secmelicheckbox As CheckedListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents zorunlulabel As Label
+    Friend WithEvents secmelilabel As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents zorunlu_ekle_buton As Button
+    Friend WithEvents zorunlu_sil_buton As Button
+    Friend WithEvents secmeli_sil_buton As Button
+    Friend WithEvents secmeli_ekle_buton As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents CheckBox6 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,168 @@
+﻿Public Class Form1
+    Dim ogrenci_sayisi As String
+    Dim zorunlu_akts, secmeli_akts As Integer
+    Dim zorunlu_dizi_akts() As Integer = {2, 3, 4, 2, 3, 2, 3, 4, 6, 2}
+    Dim secmeli_dizi_akts() As Integer = {3, 3, 2, 3, 3, 2}
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        zorunlucheckbox.CheckOnClick = True
+        secmelicheckbox.CheckOnClick = True
+        Dim kredi2 As String = "2 AKTS"
+        zorunlucheckbox.Items.Add("ATA 1007 Atatürk İlkeleri ve İnkılap Tarihi 1 A Şubesi")
+        zorunlucheckbox.Items.Add("UBP 4203 Yazılım Mimarileri A Şubesi")
+        zorunlucheckbox.Items.Add("UBP 4205 Görsel Programlama 2 A Şubesi")
+        zorunlucheckbox.Items.Add("UBP 4219 Mobil Uygulama Geliştirme 1 A Şubesi")
+        zorunlucheckbox.Items.Add("UBP 4211 Açık Kaynak İşletim Sistemi A Şubesi")
+        zorunlucheckbox.Items.Add("ATA 1008 Atatürk İlkeleri ve İnkılap Tarihi 1 B Şubesi")
+        zorunlucheckbox.Items.Add("UBP 4204 Yazılım Mimarileri B Şubesi")
+        zorunlucheckbox.Items.Add("UBP 4206 Görsel Programlama 2 B Şubesi")
+        zorunlucheckbox.Items.Add("UBP 4201 Nesne Tabanlı Programlama 1 A Şubesi")
+        zorunlucheckbox.Items.Add("UBP 4207 Mesleki Yabancı Dil 1 A Şubesi")
+        secmelicheckbox.Items.Add("UBP 4215 .NET Uygulama Geliştirme A Şubesi")
+        secmelicheckbox.Items.Add("UİŞ 4001 İşletme Yönetimine Giriş A Şubesi")
+        secmelicheckbox.Items.Add("UME 4000 Meslek Etiği A Şubesi")
+        secmelicheckbox.Items.Add("UBP 4216 .NET Uygulama Geliştirme B Şubesi")
+        secmelicheckbox.Items.Add("UİŞ 4002 İşletme Yönetimine Giriş B Şubesi")
+        secmelicheckbox.Items.Add("UME 4001 Meslek Etiği B Şubesi")
+        Dim sayisal As Boolean = False
+        Do While (sayisal = False)
+            ogrenci_sayisi = InputBox("Kaydedilecek Öğrenci Sayısını Giriniz")
+            If IsNumeric(ogrenci_sayisi) Then
+                If CInt(ogrenci_sayisi) < 0 Then
+                    MsgBox("Lütfen 0'dan Küçük Sayı Girmeyiniz")
+                Else
+                    sayisal = True
+                End If
+            ElseIf ogrenci_sayisi = "" Then
+                MsgBox("Lütfen Boş Bırakmayınız")
+            Else
+                MsgBox("Lütfen Sadece Sayısal Değer Giriniz")
+            End If
+        Loop
+        CheckBox1.Checked = True
+        CheckBox3.Checked = True
+        CheckBox5.Checked = True
+    End Sub
+
+    Private Sub TextBox2_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox2.KeyPress
+        If Not (Char.IsNumber(e.KeyChar) = True) And e.KeyChar <> ChrW(Keys.Back) Then
+            e.Handled = True
+        End If
+        TextBox2.MaxLength = 10
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If Not (Char.IsNumber(e.KeyChar) = True) And e.KeyChar <> ChrW(Keys.Back) Then
+            e.Handled = True
+        End If
+        TextBox1.MaxLength = 11
+    End Sub
+
+    Private Sub TextBox3_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox3.KeyPress
+        If Not (Char.IsLetter(e.KeyChar) = True) And e.KeyChar <> ChrW(Keys.Back) And e.KeyChar <> ChrW(Keys.Space) Then
+            e.Handled = True
+        End If
+        TextBox3.MaxLength = 20
+    End Sub
+
+    Private Sub TextBox4_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox4.KeyPress
+        If Not (Char.IsLetter(e.KeyChar) = True) And e.KeyChar <> ChrW(Keys.Back) And e.KeyChar <> ChrW(Keys.Space) Then
+            e.Handled = True
+        End If
+        TextBox4.MaxLength = 20
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            CheckBox2.Checked = False
+        Else
+            CheckBox2.Checked = True
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked = True Then
+            CheckBox1.Checked = False
+        Else
+            CheckBox1.Checked = True
+        End If
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+        If CheckBox3.Checked = True Then
+            CheckBox4.Checked = False
+        Else
+            CheckBox4.Checked = True
+        End If
+    End Sub
+
+    Private Sub CheckBox4_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox4.CheckedChanged
+        If CheckBox4.Checked = True Then
+            CheckBox3.Checked = False
+        Else
+            CheckBox3.Checked = True
+        End If
+    End Sub
+
+    Private Sub CheckBox5_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox5.CheckedChanged
+        If CheckBox5.Checked = True Then
+            CheckBox6.Checked = False
+        Else
+            CheckBox6.Checked = True
+        End If
+    End Sub
+
+    Private Sub CheckBox6_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox6.CheckedChanged
+        If CheckBox6.Checked = True Then
+            CheckBox5.Checked = False
+        Else
+            CheckBox5.Checked = True
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text.Length <> 11 Then
+            MsgBox("Lütfen 11 Haneli TC Kimlik Numaranızı Giriniz")
+            TextBox1.Focus()
+        ElseIf TextBox2.Text.Length <> 10 Then
+            MsgBox("Lütfen 10 Haneli Okul Numaranızı Giriniz")
+            TextBox2.Focus()
+        ElseIf TextBox3.Text = "" Then
+            MsgBox("Lütfen Adınızı Giriniz")
+            TextBox3.Focus()
+        ElseIf TextBox4.Text = "" Then
+            MsgBox("Lütfen Soyadınızı Giriniz")
+            TextBox4.Focus()
+        End If
+    End Sub
+
+    Private Sub zorunlu_sil_buton_Click(sender As Object, e As EventArgs) Handles zorunlu_sil_buton.Click
+        If zorunlucheckbox.CheckedItems.Count = 0 Then
+            MsgBox("Lütfen Silmek İstediğiniz Dersleri İşaretleyiniz")
+        Else
+            For secili As Integer = zorunlucheckbox.CheckedItems.Count - 1 To 0 Step -1
+                zorunlucheckbox.Items.Remove(zorunlucheckbox.CheckedItems(secili))
+            Next
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+
+    Private Sub zorunlucheckbox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles zorunlucheckbox.SelectedIndexChanged
+        If 
+    End Sub
+
+    Private Sub secmeli_sil_buton_Click(sender As Object, e As EventArgs) Handles secmeli_sil_buton.Click
+        If secmelicheckbox.CheckedItems.Count = 0 Then
+            MsgBox("Lütfen Silmek İstediğiniz Dersleri İşaretleyiniz")
+        Else
+            For secili As Integer = secmelicheckbox.CheckedItems.Count - 1 To 0 Step -1
+                secmelicheckbox.Items.Remove(secmelicheckbox.CheckedItems(secili))
+            Next
+        End If
+        'If CInt(zorunlulabel.Text) <= 22 Then
+        '    zorunlulabel.Text = CInt(zorunlulabel.Text) + zorunlu_dizi_akts(zorunlucheckbox.SelectedIndex)
+        'End If
+    End Sub
+End Class
